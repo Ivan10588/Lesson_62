68719c7e76bf39f58f203c1f32b3f64cae789de0
Ivan10588 <Ivanov_Ivan_88@mail.ru>
2026-06-28 15:25:27 +0700

    Lesson_62

--- my-projects-portfolio/portfolio/admin.py
+++ my-projects-portfolio/portfolio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'completion_date', 'created_at']
+    list_filter = ['status', 'completion_date']
+    search_fields = ['title', 'description', 'technologies']
+    date_hierarchy = 'completion_date'
--- my-projects-portfolio/portfolio/views.py
+++ my-projects-portfolio/portfolio/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+def project_list(request):
+    projects = Project.objects.filter(status='completed').order_by('-completion_date')
+    return render(request, 'portfolio/list.html', {'projects': projects})
